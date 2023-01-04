acc65eafbc47e6ed726612e3a38230bdffe525eb
Reese Sorrell <joreesesorrell@gmail.com>
2023-01-04 11:03:55 -0600

    Add menu, about, and contact content
    Add minor styling

--- src/about-page.js
+++ src/about-page.js
@@ -13,14 +13,21 @@ const populateAboutPage = () => {
     displayAdder.createDiv(about, 'Our Family', 'about-intro');
 
     const fatherTitle = "Frédregg";
-    const fatherMessage = '- Example text example text example text example text example text ';
+    const fatherMessage = `I am the father of our lovely family. I have a passion for making delicious food
+    and bringing joy to people's faces and stomachs. I serve as the head chef where I get to crack jokes all day long!`;
     const motherTitle = "Monahuevo";
-    const motherMessage = '- Example text example text example text example text example text ';
+    const motherMessage = `I'm the mother of our family and act as the floor manager at our restaurant. I get so eggcited
+    everyday that I get to work. I love serving others and helping keep our restaurant running as smoothly as possible.`;
+    const sonTitle = "Oeuf";
+    const sonMessage = `I'm the only son of the family. I wash the dishes at the restaurant, and I absolutely hate it. 
+    This restaurant is so dumb. Get me out please. I just want to eggsplore the world.`
 
     displayAdder.createDiv(about, fatherTitle, 'person-intro');
     displayAdder.createDiv(about, fatherMessage, 'person-info');
     displayAdder.createDiv(about, motherTitle, 'person-intro');
     displayAdder.createDiv(about, motherMessage, 'person-info');
+    displayAdder.createDiv(about, sonTitle, 'person-intro');
+    displayAdder.createDiv(about, sonMessage, 'person-info');
 }
 
 export {populateAboutPage as populateAboutPage};
--- src/menu-page.js
+++ src/menu-page.js
@@ -9,6 +9,11 @@ const populateMenuPage = () => {
     displayAdder.createImage(imageContainer, menuPicture, 'egg-picture');
 
     const menu = displayAdder.createDiv(parent, '', 'menu', 'menu');
+
+    displayAdder.createDiv(menu, 'All eggs are $3.44 each', 'menu-header');
+    displayAdder.createDiv(menu, `Egg Preperations: Scrambled, Boiled, Poached, Egg Omelette, Baked, Fried, Custard`, 'menu-types');
+    displayAdder.createDiv(menu, "No, we don't serve anything else. Stop asking.", 'menu-disclaimer');
+
 }
 
 export {populateMenuPage as populateMenuPage};
