a49b7433ec22c222b706c715e1acd20c965761fd
Reese Sorrell <joreesesorrell@gmail.com>
2023-01-04 16:53:53 -0600

    Finish menu tab function

--- src/menu-page.js
+++ src/menu-page.js
@@ -1,5 +1,8 @@
 import { displayAdder } from "./helper";
 import friedEggPic from "./fried-egg.png";
+import scrambledEggPic from "./scrambled-eggs.png";
+import eggsBenedictPic from "./eggs-benedict.png";
+import omelettePic from "./omelette.png";
 
 const populateMenuPage = () => {
     const parent = document.getElementById('block-container');
@@ -15,6 +18,21 @@ const populateMenuPage = () => {
     displayAdder.createDiv(firstContainer, 'Classic Fried Eggs', 'menu-item-title');
     displayAdder.createDiv(firstContainer, `Our classic fried eggs cooked to your choice of temperature in a cast iron skillet.
     They're simply eggcelent!`, 'menu-item-description');
+
+    displayAdder.createImage(secondContainer, scrambledEggPic, 'menu-item-picture');
+    displayAdder.createDiv(secondContainer, 'Scrambled Eggs', 'menu-item-title');
+    displayAdder.createDiv(secondContainer, `Soft, savory, salted, scrambled eggs are guaranteed to make you salivate. Cooked to
+    perfection and served steaming hot.`, 'menu-item-description');
+
+    displayAdder.createImage(thirdContainer, eggsBenedictPic, 'menu-item-picture');
+    displayAdder.createDiv(thirdContainer, 'Eggs Benedict', 'menu-item-title');
+    displayAdder.createDiv(thirdContainer, `English muffin, BAM. Canadian bacon, BAM. Poached egg, BAM. Perfect hollandaise sauce, BAM.
+    Trust us, you need to try it.`, 'menu-item-description');
+
+    displayAdder.createImage(fourthContainer, omelettePic, 'menu-item-picture');
+    displayAdder.createDiv(fourthContainer, 'The Omelette', 'menu-item-title');
+    displayAdder.createDiv(fourthContainer, `It's like a sandwich, but with eggs. Mix and match your favorites and we'll do what we do best.
+    Cook some eggs and crack some yolks.`, 'menu-item-description');
 }
 
 export {populateMenuPage as populateMenuPage};
